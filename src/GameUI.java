f733056189555e5828fc0f17be150b6b797880c2
ikostan <superegorius@gmail.com>
2017-08-20 16:32:02 -0700

    LabelList added

--- src/GameUI.java
+++ src/GameUI.java
@@ -1,14 +1,16 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.JComboBox;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.SystemColor;
 
 
 public class GameUI extends JFrame{
@@ -18,17 +20,6 @@ public class GameUI extends JFrame{
 	
 	private ImageIcon x, o;
 	
-	private final String TITLE ="Tic Tac Toe";
-	private final String START = "START";
-	private final String RESET = "RESET";
-	private final String CLOSE = "CLOSE";
-	private final String BTN = "button";
-	private final String SCORE = "Score:";
-	private final String zeroLbl = "0:0";
-	
-	private final String closeMeessage = "Are you sure you want to exit?";
-	private final String resetMeessage = "Are you sure you want to reset the game?";
-	
 	private JButton btnClose, btnReset, btnStart;
 	private JLabel lblScore, lblHeader;
 	private JComboBox comboBox;
@@ -37,6 +28,7 @@ public class GameUI extends JFrame{
 	private final int btnSpace = 30;
 	private int X, Y;
 	
+	private boolean isGameStarted;
 	
 	public GameUI() {
 		
@@ -44,11 +36,13 @@ public class GameUI extends JFrame{
 		setResizable(false);
 		getContentPane().setBackground(Color.LIGHT_GRAY);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle(TITLE);
+		setTitle(LableList.TITLE);
 		getContentPane().setLayout(null);
 		
 		btnArray = new JButton[]{btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8};
-			
+		
+		isGameStarted = false;
+		
 		setIcons();
 		setBtns();
 		setCloseBtn();
@@ -81,13 +75,22 @@ public class GameUI extends JFrame{
 			btn = new JButton("");
 			btn.setBounds(X, Y, btnSize, btnSize);
 			btn.setName(String.format("%d", i));
-			btn.setIcon(o);
+			btn.setBackground(Color.WHITE);
 			btn.addActionListener(new ActionListener(){
 
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					
 					System.out.println(((JButton) arg0.getSource()).getName());
+					
+					if(!isGameStarted){
+						
+						info(LableList.startMessage);
+					}
+					else{
+						
+						//TODO:
+					}
 				}
 							
 			});
@@ -114,9 +117,9 @@ public class GameUI extends JFrame{
 	//Set CLOSE button
 	private void setCloseBtn(){
 		
-		btnClose = new JButton(CLOSE);
+		btnClose = new JButton(LableList.CLOSE);
 		btnClose.setBounds(270, 425, 90, 25);
-		btnClose.setName(CLOSE + " " + BTN);
+		btnClose.setName(LableList.CLOSE + " " + LableList.BTN);
 		btnClose.addActionListener(new ActionListener(){
 
 			@Override
@@ -133,7 +136,7 @@ public class GameUI extends JFrame{
 	//Close app
 	protected void exit() {
 
-		int dialogButton = JOptionPane.showConfirmDialog(this, closeMeessage, "Confirmation Dialog", JOptionPane.YES_NO_OPTION);
+		int dialogButton = JOptionPane.showConfirmDialog(this, LableList.closeMessage, "Confirmation Dialog", JOptionPane.YES_NO_OPTION);
 		
 		if(dialogButton == 0){
 			
@@ -144,16 +147,23 @@ public class GameUI extends JFrame{
 	//Set CLOSE button
 	private void setResetBtn(){
 			
-		btnReset = new JButton(RESET);
+		btnReset = new JButton(LableList.RESET);
 		btnReset.setBounds(150, 425, 90, 25);
-		btnReset.setName(RESET  + " " + BTN);
+		btnReset.setName(LableList.RESET  + " " + LableList.BTN);
 		btnReset.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				
 				System.out.println(((JButton) arg0.getSource()).getName());
-				reset();
+				if(isGameStarted){
+					
+					reset();
+				} 
+				else{
+					
+					info(LableList.startMessage);
+				}
 			}
 						
 		});
@@ -163,7 +173,7 @@ public class GameUI extends JFrame{
 	//Reset dialog
 	private void reset() {
 		
-		int dialogButton = JOptionPane.showConfirmDialog(this, resetMeessage, "Confirmation Dialog", JOptionPane.YES_NO_OPTION);
+		int dialogButton = JOptionPane.showConfirmDialog(this, LableList.resetMessage, "Confirmation Dialog", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
 		
 		if(dialogButton == 0){
 			
@@ -171,20 +181,36 @@ public class GameUI extends JFrame{
 		}		
 	}
 	
+	//Info dialog
+	private void info(String message) {
+			
+		JOptionPane.showMessageDialog(this, message, "Information Dialog", JOptionPane.INFORMATION_MESSAGE);			
+	}
+
 	//Set START button
 	private void setStartBtn(){
 		
-		btnStart = new JButton(START);
+		btnStart = new JButton(LableList.START);
 		btnStart.setBounds(30, 425, 90, 25);
-		btnStart.setName(START + " " + BTN);
+		btnStart.setName(LableList.START + " " + LableList.BTN);
 		btnStart.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				
 				System.out.println(((JButton) arg0.getSource()).getName());
-
+				
 				//TODO:
+				
+				if(isGameStarted){
+					
+					reset();
+				}
+				else{
+					
+					isGameStarted = true;
+				}
+			
 			}
 						
 		});
@@ -202,17 +228,20 @@ public class GameUI extends JFrame{
 	//Set "Score" label
 	private void setHeaderLbl(){
 		
-		lblHeader = new JLabel(SCORE);
+		lblHeader = new JLabel(LableList.SCORE);
 		lblHeader.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHeader.setBounds(150, 390, 90, 23);
+		lblHeader.setBounds(150, 390, 90, 25);
 		getContentPane().add(lblHeader);
 	}
 	
 	//Set "Score" label
 	private void setScoreLbl(){
 		
-		lblScore = new JLabel(zeroLbl);
-		lblScore.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblScore = new JLabel(LableList.zeroLbl);
+		lblScore.setBackground(SystemColor.controlHighlight);
+		lblScore.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		lblScore.setOpaque(true); //Otherwise the background is not painted, since the default of opaque is false for JLabel.
+		lblScore.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblScore.setHorizontalAlignment(SwingConstants.CENTER);
 		lblScore.setBounds(270, 390, 90, 25);
 		getContentPane().add(lblScore);
---
+++ src/LableList.java
@@ -0,0 +1,15 @@
+
+public class LableList {
+
+	static final String TITLE ="Tic Tac Toe";
+	static final String START = "START";
+	static final String RESET = "RESET";
+	static final String CLOSE = "CLOSE";
+	static final String BTN = "button";
+	static final String SCORE = "Score:";
+	static final String zeroLbl = "0:0";
+	
+	static final String closeMessage = "Are you sure you want to exit?";
+	static final String resetMessage = "Are you sure you want to reset the game?";
+	static final String startMessage = "Please start a new game first (hit on the \"START\" button).";
+}
